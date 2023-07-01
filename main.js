c641f209414bdfa08e5f585f7c334b537daee75d
noah_vaati <noahred99@gmail.com>
2023-07-01 09:37:52 -0500

    Changed skip button to go through 5 images

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
+//used to keep track of what picture in a folder we are looking at
 imageIndex = 0;
+//max index for value above, should be 5
+maxImageIndex = 5;
+//what folder are we in? should default to most recent
+//TODO: automate this when picture changes
+imageFolder = "images/test1/";
+//image extension, jpg for now
+imageExt = ".jpg";
 
-function toggleImage(){
-    imageIndex ++;
-    imageIndex = imageIndex%2;
-
-    if(imageIndex==0)
-        changeImage("images/test0/beach0.jpg");
-    else
-        changeImage("images/test0/beach1.jpg");
-}
-
+//sets the image in imageView to whatever path is given
 function changeImage(imagePath){
     imageView.src=imagePath;
+}
+
+//gets the filename for the first image, used on body load
+function getFirstImage(){
+    //should always grab first image in folder, 0.jpg
+    return imageFolder + 0 + imageExt;
+}
+
+//functionality for the 'skip' button
+//moves to next image, and uses a guess
+function skip(){
+    if(imageIndex+1<maxImageIndex){
+        nextImage();
+    }
+}
+
+//calls changeImage to use next image in sequence
+function nextImage(){
+    imageIndex++;
+    imagePath = imageFolder + imageIndex + imageExt;
+    changeImage(imagePath);
 }
