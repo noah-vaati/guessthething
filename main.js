e2c6adbfd3b729789b87d70f3818e29b105082ee
noah_vaati <noahred99@gmail.com>
2023-07-01 16:14:48 -0500

    Added submit button, reworked skip

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ imageFolder = "images/test1/";
 //image extension, jpg for now
 imageExt = ".jpg";
 
+//answer
+//TODO: make this less static
+answer = "beach"
+
 //gets file path syntax based off of image folder 
 function getImagePath(index){
     return imageFolder + index + imageExt
@@ -31,6 +35,9 @@ function newImageSet(){
     for(i = 1; i < maxImageIndex; i++){
         toggleBtnDisable(i);
     }
+
+    //submit button should be enabled
+    document.getElementById("btnSubmit").disabled = false;
 }
 
 //functionality for the 'skip' button
@@ -39,11 +46,45 @@ function skip(){
     if(imageIndex+1<maxImageIndex){
         nextImage();
         toggleBtnDisable(imageIndex);
+    }else if(imageIndex+1>=maxImageIndex){
+        console.log("Out of guesses");
+        document.getElementById("btnSubmit").disabled = true;
+        document.getElementById("btnSkip").hidden = true;
     }
+}
+
+//submit answer, check if correct
+function submitAnswer(){
+
+    //gets value from input, then cears it
+    guess = document.getElementById("input0").value;
+    document.getElementById("input0").value = null;
+    if(guess == answer){
+        //correct answer
+        console.log("Success");
+        
+        document.getElementById("btnSubmit").disabled = true;
+    }else{
+        //wrong answer
+        console.log(imageIndex);
+        //reuse skip code
+        skip();
+
+        //apend br and guess to prevGuesses
+        node = document.createElement("br");
+        document.getElementById("prevGuesses").appendChild(node);
+        node = document.createTextNode(guess);
+        document.getElementById("prevGuesses").appendChild(node);
+
+        
+    }
+
+    
 }
 
 //calls changeImage to use next image in sequence
 function nextImage(){
     imageIndex++;
     changeImage(getImagePath(imageIndex));
+    //check if max guesses
 }
